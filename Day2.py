259361531b4409c185c52c227c5ee9aedf503ba1
Liam <thisamazingnow@gmail.com>
2022-12-14 09:32:16 -0500

    updates

--- Day2.py
+++ Day2.py
@@ -2,16 +2,16 @@ testdate = """A Y
 B X
 C Z"""
 
-lines = testdate.split("\n")
 
-value_assignment = {'A':0, 'B':1, 'C':2,'X':0, 'Y':1, 'Z':2}
+value_assignment = {'A':[0,1], 'B':[1,2], 'C':[2,3],'X':[0,1], 'Y':[1,2], 'Z':[2,3]}
 
-result_matrix = [[0,2,1],
-                 [1,0,2],
-                 [2,1,0],]
+result_matrix = [[3,0,6],
+                 [6,3,0], 
+                 [0,6,3],]
 
 # 0 is rock 1 is paper 2 is scissor
 # X is rock Y is paper Z is scissor
+# 1 point for rock, 2 for paper, 3 for scissors
 
 def GetInputFile():
     return input("File Name:")
@@ -19,15 +19,22 @@ def GetInputFile():
 def ReadFile (filename):
     return open(filename).read()
 
-def victory_decision():
+def victory_decision(input):
+    total_score = 0
+    lines = input.split("\n")
     for line in lines:
-        values = [str(lines).split(" ")]
+        point_value = 0
+        values = line.split(" ")
 
-        choice_index_you = value_assignment.get(str(values[0]))
-        choice_index_elf = value_assignment.get(str(values[1]))
+        choice_index_you = value_assignment.get(str(values[1]))
+        choice_index_elf = value_assignment.get(str(values[0]))
         
-        game_result = result_matrix[choice_index_you][choice_index_elf]
+        game_result = result_matrix[choice_index_you[0]][choice_index_elf[0]]
+        point_value = game_result + choice_index_you[1]
+        total_score = total_score + point_value
+    return total_score
 
-        return game_result
 
-print(victory_decision())
+def process():
+    return victory_decision(ReadFile(GetInputFile()))
+print(f"{process()}")
